0944e1fd1abfb70376f8dfc88a537ed08dd19484
Philippe Martin <phmartin@redhat.com>
2022-03-22 13:09:11 +0100

    odo devfile pull

--- pkg/odo/cli/devfile/pull.go
+++ pkg/odo/cli/devfile/pull.go
@@ -3,8 +3,11 @@ package devfile
 import (
 	"errors"
 	"fmt"
+	"os"
 
+	"github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
 	"github.com/devfile/library/pkg/devfile/parser"
+	"github.com/devfile/library/pkg/devfile/parser/data/v2/common"
 	"github.com/redhat-developer/odo/pkg/devfile"
 	"github.com/redhat-developer/odo/pkg/devfile/location"
 	"github.com/redhat-developer/odo/pkg/devfile/validate"
@@ -18,8 +21,12 @@ import (
 const PullRecommendedCommandName = "pull"
 
 type PullOptions struct {
-	clientset  *clientset.Clientset
-	devfileObj parser.DevfileObj
+	cwd            string
+	clientset      *clientset.Clientset
+	devfilePath    string
+	devfileObj     parser.DevfileObj
+	projectName    string
+	starterProject v1alpha2.StarterProject
 }
 
 // NewAlizerOptions creates a new AlizerOptions instance
@@ -32,30 +39,77 @@ func (o *PullOptions) SetClientset(clientset *clientset.Clientset) {
 }
 
 func (o *PullOptions) Complete(cmdline cmdline.Cmdline, args []string) (err error) {
-	devfilePath := location.DevfileLocation("")
-	isDevfile := util.CheckPathExists(devfilePath)
+	o.devfilePath = location.DevfileLocation("")
+	isDevfile := util.CheckPathExists(o.devfilePath)
 	if !isDevfile {
 		return errors.New("no devfile found")
 	}
 
-	o.devfileObj, err = devfile.ParseAndValidateFromFile(devfilePath)
+	o.devfileObj, err = devfile.ParseAndValidateFromFile(o.devfilePath)
 	if err != nil {
-		return fmt.Errorf("failed to parse the devfile %s: %w", devfilePath, err)
+		return fmt.Errorf("failed to parse the devfile %s: %w", o.devfilePath, err)
 	}
 	err = validate.ValidateDevfileData(o.devfileObj.Data)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	if len(args) != 1 {
+		return errors.New("you should specify a starter project name")
+	}
+	o.projectName = args[0]
+
+	o.cwd, err = os.Getwd()
+	return err
 }
 
 func (o *PullOptions) Validate() error {
+	projects, err := o.devfileObj.Data.GetStarterProjects(common.DevfileOptions{})
+	if err != nil {
+		return err
+	}
+	found := false
+	for _, project := range projects {
+		if project.Name == o.projectName {
+			o.starterProject = project
+			found = true
+			break
+		}
+	}
+	if !found {
+		return fmt.Errorf("project %q not found in devfile", o.projectName)
+	}
 	return nil
 }
 
 // Run contains the logic for the odo command
 func (o *PullOptions) Run() (err error) {
+	err = o.clientset.InitClient.DownloadStarterProject(&o.starterProject, o.cwd)
+	if err != nil {
+		return err
+	}
+	if !util.CheckPathExists(o.devfilePath) {
+		// devfile has been erased during starter project download
+		err = o.devfileObj.WriteYamlDevfile()
+		if err != nil {
+			return err
+		}
+	} else {
+		// devfile has been replaced or has not been erased during starter project download
+		name := o.devfileObj.GetMetadataName()
+		o.devfileObj, err = devfile.ParseAndValidateFromFile(o.devfilePath)
+		if err != nil {
+			return fmt.Errorf("failed to parse the devfile %s: %w", o.devfilePath, err)
+		}
+		err = validate.ValidateDevfileData(o.devfileObj.Data)
+		if err != nil {
+			return err
+		}
+		err = o.devfileObj.SetMetadataName(name)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -64,11 +118,13 @@ func NewCmdPull(name, fullName string) *cobra.Command {
 	devfileCmd := &cobra.Command{
 		Use:         name,
 		Long:        "Pull a starter project defined in a devfile",
-		Args:        cobra.MaximumNArgs(0),
+		Args:        cobra.ExactArgs(1),
 		Annotations: map[string]string{},
 		Run: func(cmd *cobra.Command, args []string) {
 			genericclioptions.GenericRun(o, cmd, args)
 		},
 	}
+	clientset.Add(devfileCmd, clientset.INIT)
+	devfileCmd.Annotations["machineoutput"] = "json"
 	return devfileCmd
 }
